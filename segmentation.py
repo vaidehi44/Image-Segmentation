e2d3737f2d5bbd8b39dcdabdafc6642226e43c92
Vaidehi <vaidehiatpadkar1@gmail.com>
2021-11-07 12:36:30 +0530

    Finalize code with optimizing computation time

--- segmentation.py
+++ segmentation.py
@@ -2,6 +2,7 @@ from PIL import Image
 import random
 import numpy as np
 
+
 img = Image.open("Image.jpg", 'r')
 pixels = list(img.getdata())
 
@@ -14,52 +15,48 @@ for i in range(k):
     b_val = random.randint(0, 255)
     centers.append([r_val, g_val, b_val])
     
-def distance(center, data_pt):
-    dis=0
-    for i in range(3):
-        diff = center[i]-data_pt[i]
-        dis += diff**2
 
-    return dis
+def distance(center):
+    diff = np.array(pixels)-center
+    distances = [i[0]**2+i[1]**2+i[2]**2 for i in diff]
+    return distances
 
 pixels_clusters = np.zeros(len(pixels))
 
 def assign_pixel_clusters():
+    distances = []
+    for c in range(k):
+       distances.append(distance(centers[c]))
     for i in range(len(pixels)):
         cluster = 0
-        min_dis = distance(centers[0], pixels[i])
+        min_dis = distances[0][i]
         for j in range(1,k):
-            dis = distance(centers[j], pixels[i])
-            if dis<min_dis:
-                min_dis=dis
-                cluster=j
-        pixels_clusters[i]=cluster
+            dist = distances[j][i]
+            if dist<min_dis:
+                min_dis = dist
+                cluster = j
+        pixels_clusters[i] = cluster
+            
         
 def find_new_centers():
     for i in range(k):
-        count=0
-        add=np.array([0,0,0])
-        for j in range(len(pixels_clusters)):
-            if pixels_clusters[j]==i:
-                count+=1
-                add+=pixels[j]
-        mean = add/count
+        indices = np.where(pixels_clusters==i)
+        mean = np.array(pixels)[indices].mean(axis=0)
         mean = mean.astype(np.int32())
-        centers[i]=mean
+        centers[i] = mean
         
-for itr in range(20):
+        
+for itr in range(10):
     assign_pixel_clusters()
     find_new_centers()
 
 
-new_pixels = []
-
 def segmented_image():
+    new_pixels = []
     for i in pixels_clusters:
         new_pixels.append(tuple(centers[int(i)]))
     new_image = Image.new(img.mode,img.size)
     new_image.putdata(new_pixels)
-    new_image.save("result.png")
+    new_image.save("result_"+str(k)+".png")
 
 segmented_image()
-        
